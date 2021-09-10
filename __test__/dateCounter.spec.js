49eecc1acfc5f3d5d662663c7917577a8dcdaaef
xxulverxx <35438607+rjrm84@users.noreply.github.com>
2021-09-10 17:52:17 -0300

    gitignore

---
+++ __test__/dateCounter.spec.js
@@ -0,0 +1,11 @@
+import { dateCounter } from '../src/client/js/dateCounter';
+import { today } from '../src/client/js/today'
+
+describe('testing the date counter functionality', () => {
+    test('tests dateCounter() function', () => {
+        //    input
+        // expected output
+        // expect()
+        expect(dateCounter).toBeDefined();
+    })
+})
